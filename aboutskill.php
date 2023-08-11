42d2bbc5b3663f472d5c67e71a82f1b71c32911a
jagmohan123 <jagmohanrai082@gmail.com>
2023-08-11 15:12:14 +0530

    aboutskill page added

---
+++ aboutskill.php
@@ -0,0 +1,48 @@
+<?php include('headerskill.php')?>
+
+<section class="w3l-skill-breadcrum">
+  <div class="breadcrum">
+   
+  </div>
+</section>
+<section class="w3l-about1" id="about">
+  <div id="cwp23-block" class="py-5">
+    <div class="container py-lg-5">
+      <div class="row cwp23-content align-items-center">
+        <div class="col-lg-6 cwp23-text">
+          <div class="cwp23-title">
+            <h3>Welcome to the
+              Professional Education Theme! </h3>
+          </div>
+          <div class="cwp23-text-cols">
+            <div class="column">
+              <span class="fa fa-smile-o" aria-hidden="true"></span>
+              <a href="services.html">Group Seminars</a>
+              <p>consectetur adipisicing elit, sed do eiusmod tempor </p>
+            </div>
+            <div class="column">
+              <span class="fa fa-graduation-cap" aria-hidden="true"></span>
+              <a href="services.html">Trending Courses</a>
+              <p>consectetur adipisicing elit, sed do eiusmod tempor </p>
+            </div>
+            <div class="column">
+              <span class="fa fa-history" aria-hidden="true"></span>
+              <a href="services.html">Large Library</a>
+              <p>consectetur adipisicing elit, sed do eiusmod tempor </p>
+            </div>
+            <div class="column">
+              <span class="fa fa-users" aria-hidden="true"></span>
+              <a href="services.html">Expert Teachers</a>
+              <p>consectetur adipisicing elit, sed do eiusmod tempor </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 mt-lg-0 mt-5 cwp23-img">
+          <img src="assets/images/g6.jpg" class="img-fluid" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include('footerskill.php')?>
