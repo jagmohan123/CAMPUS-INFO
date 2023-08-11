376221093945973c1c02c1dc9cebad553283bd5a
jagmohan123 <jagmohanrai082@gmail.com>
2023-08-11 15:14:31 +0530

    serviceskill page added

---
+++ serviceskill.php
@@ -0,0 +1,71 @@
+<?php  include('headerskill.php'); ?>
+
+    <!--  servcies section -->
+  <div class="w3l-services1 pt-5" id="services">
+   < <div class="container pt-lg-3">
+      <div class="aboutgrids row">
+        <div class="col-lg-6 aboutgrid2">
+          <img src="assets/images/g1.jpg" alt="about image" class="img-fluid" />
+        </div>
+        <div class="col-lg-6 aboutgrid1 my-lg-0 my-5">
+          <h4>Having fun while learning is possible</h4>
+          <p>Lorem ipsum dolor sit amet adipisicing elit. Quae blanditiis porro facere qui
+            impedit dolor doloribus.</p>
+          <ul class="services-list mt-4">
+            <li>Learn Courses Online</li>
+            <li>Highly Qualified Teachers</li>
+            <li>Book Library & Stores</li>
+            <li>Apply for Scholarship</li>
+          </ul>
+
+          <a href="contact.html" class="btn btn-outline-secondary theme-button">Know More</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="aboutbottom py-5">
+      <div class="container py-lg-3">
+        <div class="bottomgrids row no-gutters">
+          <div class="col-lg-4 col-md-6 bottomgrid1 mt-lg-0 mt-0">
+            <span class="fa fa-phone"></span>
+            <h4><a class="service-title" href="#url">Free Call Support</a></h4>
+            <p class="service-text">Donec et venenatis libero. Fusceing dapibus pulvinar tincidunt. Proin
+              maximus ipsum ut scelerisque.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 bottomgrid1 mt-md-0 mt-5">
+            <span class="fa fa-users"></span>
+            <h4><a class="service-title" href="#url">Highly Qualified Teachers</a></h4>
+            <p class="service-text">Donec et venenatis libero. Fusceing dapibus pulvinar tincidunt. Proin
+              maximus ipsum ut scelerisque.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 bottomgrid1 mt-lg-0 mt-5">
+            <span class="fa fa-book"></span>
+            <h4><a class="service-title" href="#url">Book Library & Stores</a></h4>
+            <p class="service-text">Donec et venenatis libero. Fusceing dapibus pulvinar tincidunt. Proin
+              maximus ipsum ut scelerisque.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 bottomgrid1 mt-5">
+            <span class="fa fa-smile-o"></span>
+            <h4><a class="service-title" href="#url">The best discount</a></h4>
+            <p class="service-text">Donec et venenatis libero. Fusceing dapibus pulvinar tincidunt. Proin
+              maximus ipsum ut scelerisque.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 bottomgrid1 mt-5">
+            <span class="fa fa-laptop"></span>
+            <h4><a class="service-title" href="#url">Learn Courses Online</a></h4>
+            <p class="service-text">Donec et venenatis libero. Fusceing dapibus pulvinar tincidunt. Proin
+              maximus ipsum ut scelerisque.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 bottomgrid1 mt-5">
+            <span class="fa fa-phone"></span>
+            <h4><a class="service-title" href="#url">Free Call Support</a></h4>
+            <p class="service-text">Donec et venenatis libero. Fusceing dapibus pulvinar tincidunt. Proin
+              maximus ipsum ut scelerisque.</p>
+          </div>
+          <div class="clearfix"> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--  //servcies section -->
+<?php  include('footerskill.php'); ?>
